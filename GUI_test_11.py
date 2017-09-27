9d60bced686e6f7542d4c2f4381f894829caca73
catsymptote <catsymptote@gmail.com>
2017-09-27 15:59:19 +0200

    Display graphics/photos. Python TKinter tutorials finished.

---
+++ GUI_test_11.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+
+# Start ----------
+root_window = Tk()  # Window constructor / blank window
+# Start ----------
+
+
+photo_1 = PhotoImage(file="img/img_1.png")
+label_1 = Label(root_window, image=photo_1)
+label_1.pack()
+
+photo_2 = PhotoImage(file="img/img_2.png")
+label_2 = Label(root_window, image=photo_2)
+label_2.pack()
+
+
+# Stop ---------------
+root_window.mainloop()
+# Stop ---------------
